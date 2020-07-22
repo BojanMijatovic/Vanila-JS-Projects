ec01a8e69924b57900e553a94612eea5e98f43de
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-22 12:42:27 +0200

    Data Types

---
+++ starting with JS/main.js
@@ -0,0 +1,29 @@
+let name = 'Rex';
+let address = 'main street';
+let city = 'Valencia';
+console.log(`${name} from ${city} address is ${address}.`);
+
+const num1 = 3;
+const num2 = 332.2;
+console.log(num1);
+
+const t = '1' + '1';
+console.log(t); // return string
+
+const n3 = num2 + '1';
+console.log(n3); // return num with added string
+
+let a = 5;
+a++;
+console.log(a);
+a += 3;
+console.log(a);
+a--;
+console.log(a);
+
+// Implicit Type Conversion
+
+let r = '5';
+console.log(r);
+r = parseInt(r);
+console.log(r);  
