17b7b3e9e34214baf393f7599ebff0015370cf4e
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-22 13:15:41 +0200

    Functions - Parameters, Arguments

--- starting with JS/main.js
+++ starting with JS/main.js
@@ -1,29 +1,21 @@
-let name = 'Rex';
-let address = 'main street';
-let city = 'Valencia';
-console.log(`${name} from ${city} address is ${address}.`);
 
-const num1 = 3;
-const num2 = 332.2;
-console.log(num1);
+// const list = ['Ted', 'Liz', 'Alex', 'Bob'];
+// console.log(list[list.length - 1]);
 
-const t = '1' + '1';
-console.log(t); // return string
+// list[4] = 'new friend';
+// console.log(list);
 
-const n3 = num2 + '1';
-console.log(n3); // return num with added string
+// list[4] = 'Freddy';
+// console.log(list);
 
-let a = 5;
-a++;
-console.log(a);
-a += 3;
-console.log(a);
-a--;
-console.log(a);
+function hello() {
+  return console.log(`Welcome`);
+}
 
-// Implicit Type Conversion
+hello();
 
-let r = '5';
-console.log(r);
-r = parseInt(r);
-console.log(r);  
+function helloGest(name) {
+  return console.log(`Welcome ${name}`);
+}
+
+helloGest('Bob');
