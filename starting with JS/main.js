ceb368f92689edfec0623e4828cdc609829a62ab
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-23 13:07:09 +0200

    DOM - Intro

--- starting with JS/main.js
+++ starting with JS/main.js
@@ -1,20 +1,42 @@
-const list = ['books', 'cars', 'fruits', 'drinks', 'beer'];
+// const list = ['books', 'cars', 'fruits', 'drinks', 'beer'];
 
-const people = [
-  { name: 'Alex', job: 'developer', age: 32 },
-  { name: 'Tim', job: 'solider', age: 25 },
-  { name: 'Rex', job: 'manager', age: 40 }
-]
+// const people = [
+//   { name: 'Alex', job: 'developer', age: 32 },
+//   { name: 'Tim', job: 'solider', age: 25 },
+//   { name: 'Rex', job: 'manager', age: 40 }
+// ]
 
-const peopleAge = people.map(function (person) {
-  return person.age;
-})
+// const peopleAge = people.map(function (person) {
+//   return person.age;                                        // map function return new arr
+// })
+// console.log(peopleAge);
 
-console.log(peopleAge);
+
+// const showNames = people.map(function (person) {
+//   let test = document.createElement('p');
+//   test.innerText = `Name: ${person.name}`;
+//   document.body.append(test);
+// })
 
 
-const showNames = people.map(function (person) {
-  let test = document.createElement('p');
-  test.innerText = `Name: ${person.name}`;
-  document.body.append(test);
-})
+// const olderPeople = people.filter(function (person) {
+//   return person.age > 30;                                        // filter return new array
+// })
+// console.log(olderPeople);
+
+
+// const findPeople = people.find(function (person) {  // find return obj
+//   return person.name === 'Rex';
+// })
+// console.log(findPeople);
+
+
+// const allAge = people.reduce(function (acc, curr) {
+//   return acc += curr.age;
+// }, 0)
+// console.log(allAge);
+
+// const date = new Date();
+// console.log(date.toDateString());
+
+// document.body.append(date.toDateString())
