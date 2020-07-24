29ed563451255f8337991b36623a4649a0b18cac
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-24 12:34:46 +0200

    Click Event

--- starting with JS/main.js
+++ starting with JS/main.js
@@ -1,31 +1,12 @@
 
-
-const h2 = document.querySelector('#dom');
-
-h2.classList.add('subtitle', 'addNew');
+const button = document.getElementById('btn');
 
 
-const list = document.querySelector('.list');
-console.log(list);
-const newList = document.createElement('li');
-
-newList.textContent = 'New Item from dom';
-newList.classList.add('list');
-list.append(newList);
 
 
-const text = document.createElement('p');
-text.textContent = 'Test is for practice';
-text.classList.add('list-item');
-list.append(text);
-
-const newDiv = document.querySelector('.newItems');
-
-newDiv.innerHTML = `<ul class="list"> 
-<li>This is all from dom</li>
-<li>This is all from inner HTML</li>
-<li>This is all from dom</li>
-</ul>`;
-
-
-document.body.append(newDiv);
+button.addEventListener('click', function () {
+  const liItem = document.querySelector('.list-item');
+  liItem.textContent = 'This is from btn click event';
+  liItem.classList.toggle('remove-item');
+  console.log('Btn');
+})
