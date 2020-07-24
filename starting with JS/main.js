d908a8300680eeb3550946ec974b00b179ca7107
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-24 18:08:54 +0200

    form submit

--- starting with JS/main.js
+++ starting with JS/main.js
@@ -1,35 +1,7 @@
+const form = document.querySelector('#form');
 
-const button = document.getElementById('btn');
-
-
-function changeColorAndText() {
-  const liItem = document.querySelector('.list-item');
-  liItem.textContent = 'This is from btn click event';
-  liItem.classList.toggle('remove-item');
-  console.log('Btn');
-}
-
-
-button.addEventListener('click', changeColorAndText);
-
-
-const textV = document.querySelector('#textValue');
-
-function showValue(e) {
-  console.log(e.target);
-}
-
-
-textV.addEventListener('input', showValue);
-
-
-const form = document.getElementById('form');
-
-const btn = document.getElementById('btn-form');
-
-btn.addEventListener('submit', function (e) {
+form.addEventListener('submit', function (e) {
+  const text = document.querySelector('#text').value;
   e.preventDefault();
-  console.log(e.target.value);
-
+  console.log(text);
 })
-
