a787c439be7349adffe8757e51d476ee72d3d009
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-24 17:39:42 +0200

    Example

--- starting with JS/main.js
+++ starting with JS/main.js
@@ -2,11 +2,34 @@
 const button = document.getElementById('btn');
 
 
-
-
-button.addEventListener('click', function () {
+function changeColorAndText() {
   const liItem = document.querySelector('.list-item');
   liItem.textContent = 'This is from btn click event';
   liItem.classList.toggle('remove-item');
   console.log('Btn');
+}
+
+
+button.addEventListener('click', changeColorAndText);
+
+
+const textV = document.querySelector('#textValue');
+
+function showValue(e) {
+  console.log(e.target);
+}
+
+
+textV.addEventListener('input', showValue);
+
+
+const form = document.getElementById('form');
+
+const btn = document.getElementById('btn-form');
+
+btn.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(e.target.value);
+
 })
+
