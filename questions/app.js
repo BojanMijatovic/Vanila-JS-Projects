13755abfc867abe3fbe751792ec62eb446233e86
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-27 17:48:15 +0200

    Traversing the DOM

--- questions/app.js
+++ questions/app.js
@@ -1,2 +1,12 @@
 //using selectors inside the element
 // traversing the dom
+
+
+const questionBtns = document.querySelectorAll('.question-btn');
+
+questionBtns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  })
+})
