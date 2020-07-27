da8f5dd47a726fa49b8a98e47aa78e6c47af96b8
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-27 11:15:47 +0200

    Simple change colors

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+function changeColor() {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  colorSpan.textContent = colors[randomNumber];
+}
+
+btn.addEventListener('click', changeColor);
