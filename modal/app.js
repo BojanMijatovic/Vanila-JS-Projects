8d90e3af556771055a187a1ef43ff1c75faff22c
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-27 17:08:47 +0200

    modal js

--- modal/app.js
+++ modal/app.js
@@ -2,3 +2,18 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const modalBtn = document.querySelector('.modal-btn');
+const overlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+
+modalBtn.addEventListener('click', function () {
+  overlay.classList.add('open-modal');
+})
+
+
+closeBtn.addEventListener('click', function () {
+  overlay.classList.remove('open-modal');
+})
