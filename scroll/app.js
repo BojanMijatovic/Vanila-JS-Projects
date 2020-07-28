8812529cc4ec542ca9dbd5b421cf501fdb77fa24
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-28 14:05:10 +0200

    add js code

--- scroll/app.js
+++ scroll/app.js
@@ -12,9 +12,69 @@ const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
+
 // ********** fixed navbar ************
 navToggle.addEventListener('click', function () {
-  linksContainer.classList.toggle('show-links');
+  // linksContainer.classList.toggle('show-links');
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 })
+
+
+const navbar = document.querySelector('#nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+
+  //  top link class
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+})
+
+
+
 // ********** smooth scroll ************
-// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    //  navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+
+    //  calculate height
+    const navHeight = navbar.getBoundingClientRect().height;
+    const navContainer = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position -= navHeight;
+    }
+
+    if (navHeight > 82) {
+      position += navContainer;
+    }
+
+    window.scrollTo({ left: 0, top: position });
+    linksContainer.style.height = 0;
+  })
+})
